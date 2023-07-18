ca032f5014c157c7bb7fcfe0f2d648ade986fa56
etiry <7997357+etiry@users.noreply.github.com>
2023-07-18 12:46:51 -0400

    Added code to pass tests

--- main.js
+++ main.js
@@ -1,1 +1,53 @@
 // write your code here to make the tests pass
+var Library = function () {
+  books = [];
+
+  addBook = function (book) {
+    books.push(book);
+  };
+
+  checkOutBook = function (book) {
+    if (books.includes(book)) {
+      book.setAttribute('checkedOut', true);
+    } else {
+      console.log('The library doesn\'t have that book!');
+    }
+  };
+
+  returnBook = function (book) {
+    if (books.includes(book)) {
+      book.setAttribute('checkedOut', false);
+    } else {
+      console.log('The library doesn\'t have that book!');
+    }
+  };
+
+  return {
+    addBook,
+    checkOutBook,
+    returnBook
+  }
+};
+
+var Book = function (title, author) {
+  var attributes = {
+    title: title,
+    author: author,
+    checkedOut: false,
+  }
+
+  getAttribute = function (attribute) {
+    return attributes[attribute];
+  };
+
+  setAttribute = function (attribute, value) {
+    if (attributes.hasOwnProperty(attribute)) {
+      attributes[attribute] = value;
+    }
+  };
+
+  return {
+    getAttribute,
+    setAttribute
+  }
+};
